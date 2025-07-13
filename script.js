7c19fc7d71b5ad9ffbf3a0874244ac88c3bfded5
dlgav987 <soydiiana.da@gmail.com>
2025-07-12 19:08:23 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarInfo(materia) {
+  const infoBox = document.getElementById("info");
+  if (materia === "mat1") {
+    infoBox.innerHTML = "<strong>Matemáticas I:</strong> Introducción al álgebra. Prerrequisito: Ninguno.";
+  }
+}
